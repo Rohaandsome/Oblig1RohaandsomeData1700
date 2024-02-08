0a282b02433a9dcb09a82562febfc90b9efca978
Rohaandsome <rohaan2403@gmail.com>
2024-02-09 00:14:50 +0100

    Endelig ferdig. Non stop jobbing i 9 timer. Burde tatt mindre tid fr.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -21,6 +21,52 @@ function registrerBillett(){
     const tlf = document.getElementById("tlf").value;
     const mail = document.getElementById("mail").value;
 
+    const feilFilm = document.getElementById("feilFilm");
+    const feilMail = document.getElementById("feilMail");
+    // søkte opp dette under for å finne måte å feilsøke mail.
+    const sjekkMail = /^[\w\.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+    const feilSeter = document.getElementById("feilSeter");
+    const feilFnavn = document.getElementById("feilFnavn");
+    const feilEnavn = document.getElementById("feilEnavn");
+    const feilTlf = document.getElementById("feilTlf");
+
+    if (film === "") {
+        feilFilm.textContent = "Velg en film!";
+        slettInput();
+        return;
+    } else feilFilm.textContent = "";
+
+    if (isNaN(seter) || !Number.isInteger(Number(seter)) || Number(seter) <=  0){
+        feilSeter.textContent = "Skriv antall seter!";
+        slettInput();
+        return;
+    } else feilSeter.textContent = "";
+
+    if (typeof fornavn != "string" || fornavn.trim().length === 0|| /\d/.test(fornavn)){
+        feilFnavn.textContent = "Skriv inn ett gyldig fornavn!";
+        slettInput();
+        return;
+    } else feilFnavn.textContent ="";
+
+    if (typeof etternavn != "string" || etternavn.trim().length === 0 || /\d/.test(etternavn)) {
+        feilEnavn.textContent = "Skriv inn ett gyldig etternavn!";
+        slettInput();
+        return;
+    } else feilEnavn.textContent = "";
+
+    if (isNaN(tlf || !Number.isFinite(tlf))){
+        feilTlf.textContent = "Skriv inn ett gyldig telefonummer!";
+        slettInput();
+        return;
+    } else feilTlf.textContent = "";
+
+    if (!sjekkMail.test(mail)) {
+        feilMail.textContent = "Skriv en gyldig e-postadresse!";
+        slettInput();
+        return;
+    } else feilMail.textContent = "";
+
+
     const enBillett = {
         film: film,
         seter: seter,
@@ -30,19 +76,24 @@ function registrerBillett(){
         mail: mail
     };
     billetter.push(enBillett);
-    document.getElementById("film").value="";
-    document.getElementById("seter").value="";
-    document.getElementById("fornavn").value="";
-    document.getElementById("etternavn").value="";
-    document.getElementById("tlf").value="";
-    document.getElementById("mail").value="";
+    slettInput()
     visBilletter()
 }
 
+function slettInput() {
+    document.getElementById("film").value = "";
+    document.getElementById("seter").value = "";
+    document.getElementById("fornavn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("tlf").value = "";
+    document.getElementById("mail").value = "";
 
 
+
+
+
+}
 function slettAlt() {
     billetter = [];
     visBilletter();
 }
-
